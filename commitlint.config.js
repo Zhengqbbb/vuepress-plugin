e464140f90c4418f225649fc43a51d8899c9d3a7
Zhengqbbb <1074059947@qq.com>
2022-02-22 15:19:03 +0800

    ci: update config for cz-git

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   prompt: {
     messages: {
-      type: "选择你的提交类型    | Select the type of change that you\'re committing:",
+      type: "选择你的提交类型   | Select the type of change that you\'re committing:",
       scope: "选择一个模块范围(可选) | Denote the SCOPE of this change (optional)",
       customScope: "自定义修改模块名 | Denote the SCOPE of this change:",
       subject: "简短说明 | Write a SHORT, IMPERATIVE tense description of the change:\n",
@@ -14,17 +14,17 @@ module.exports = {
       confirmCommit: "是否提交或修改commit | Are you sure you want to proceed with the commit above?"
     },
     types: [
-      {value: 'docs',     name: 'docs:     文档更新  | Documentation only changes'},
-      {value: 'feat',     name: 'feat:     新特性    | A new feature'},
-      {value: 'fix',      name: 'fix:      修复缺陷  | A bug fix'},
-      {value: 'style',    name: 'style:    样式更改  | Changes that do not affect the meaning of the code'},
-      {value: 'refactor', name: 'refactor: 代码重构  | A code change that neither fixes a bug nor adds a feature'},
-      {value: 'perf',     name: 'perf:     性能提升  | A code change that improves performance'},
-      {value: 'test',     name: 'test:     测试相关  | Adding missing tests or correcting existing tests'},
-      {value: 'build',    name: 'build:    构建相关  | Changes that affect the build system or external dependencies (example scopes: gulp, webpack, npm)'},
-      {value: 'ci',       name: 'ci:       命令行工具| Changes to our CI configuration files and scripts'},
-      {value: 'revert',   name: 'revert:   回退代码  | Revert to a commit'},
-      {value: 'chore',    name: 'chore:    其他修改  | Other changes that do not modify src or test files'},
+      {value: 'docs',     name: 'docs:     文档更新 | Documentation only changes'},
+      {value: 'feat',     name: 'feat:     新增功能 | A new feature'},
+      {value: 'fix',      name: 'fix:      修复缺陷 | A bug fix'},
+      {value: 'style',    name: 'style:    样式更改 | Changes that do not affect the meaning of the code'},
+      {value: 'refactor', name: 'refactor: 代码重构 | A code change that neither fixes a bug nor adds a feature'},
+      {value: 'perf',     name: 'perf:     性能提升 | A code change that improves performance'},
+      {value: 'test',     name: 'test:     测试相关 | Adding missing tests or correcting existing tests'},
+      {value: 'build',    name: 'build:    构建相关 | Changes that affect the build system or external dependencies (example scopes: gulp, webpack, npm)'},
+      {value: 'ci',       name: 'ci:       持续集成 | Changes to our CI configuration files and scripts'},
+      {value: 'revert',   name: 'revert:   回退代码 | Revert to a commit'},
+      {value: 'chore',    name: 'chore:    其他修改 | Other changes that do not modify src or test files'},
     ],
     scopes: [
       {name: 'blog'},
