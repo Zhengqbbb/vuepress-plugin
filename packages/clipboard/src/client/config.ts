2c47e917c543bbb28d0a7e4ae4d63ce1ec3778fe
Zhengqbbb <1074059947@qq.com>
2022-05-25 23:50:13 +0800

    style(plugin-clipboard): update setup func

--- packages/clipboard/src/client/config.ts
+++ packages/clipboard/src/client/config.ts
@@ -1,6 +1,6 @@
 import { defineClientConfig } from "@vuepress/client";
 import CodeCopy from "./composables/CodeCopy.vue";
-import { setupCopyCode } from "./composables/codeCopy";
+import { setupCopyCode } from "./composables/useCodeCopy";
 
 import "./styles/main.css";
 
