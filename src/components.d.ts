232b124cece1fb994bed6dcde9deb0674323c249
Zhengqbbb <1074059947@qq.com>
2022-08-22 11:25:33 +0800

    docs: use netlify replace ghpage

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,7 +7,6 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    Counter: typeof import('./components/Counter.vue')['default']
     Footer: typeof import('./components/Footer.vue')['default']
     README: typeof import('./components/README.md')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
