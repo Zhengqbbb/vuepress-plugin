fd39ac99d8bda9d978014f56b9dac05c7f11a55d
Zhengqbbb <1074059947@qq.com>
2023-03-09 01:20:26 +0800

    chore: update config

--- docs/.vuepress/configs/head.ts
+++ docs/.vuepress/configs/head.ts
@@ -3,4 +3,5 @@ import type { HeadConfig } from '@vuepress/core'
 export const head: HeadConfig[] = [
   ['meta', { name: 'google-site-verification', content: 'j6cNWewUj3QcJBpdv48t67XoDxdlKPQ6dwKgXg9Nigo' }],
   ['meta', { name: 'baidu-site-verification', content: 'code-JiFVTLHmcI' }],
+  ['script', { 'async': true, 'defer': true, 'data-website-id': `${process.env.UMAIMI_ID || ''}`, 'src': `${process.env.UMAIMI_ENDPOINT || ''}` }],
 ]
